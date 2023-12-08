792c81c33aa20a6eddfcf1572dca066fdaf563c6
Xianglmoffitt <xiang.liu@moffitt.org>
2023-12-08 11:54:14 -0500

    update

--- ggplot_chrmosome.R
+++ ggplot_chrmosome.R
@@ -1,4 +1,4 @@
-setwd("~/Projects/super_enhancer/se_data_portal/new_analysis/")
+setwd("~/Projects/super_enhancer/se_data_portal/new_analysis/cSEAdb_revision/")
 library(GenomicRanges)
 library(ggplot2)
 library(tidyr)
@@ -6,7 +6,7 @@ library(data.table)
 library(RColorBrewer)
 library(ggchicklet)
 
-cSEAdb <- readRDS("results/cSEAdb_v4.rds")
+cSEAdb <- readRDS("results/cSEAdb_r1.rds")
 
 # chrom background
 cancer_cell_meta <- cSEAdb$cell_cancer_meta
@@ -96,7 +96,7 @@ chr_plot_df$group[which(chr_plot_df$se_name %in% se_both)] <- "both"
 
 chr_plot_df_2 <- merge(chr_plot_df,chrom_size,by="V1")
 
-chr_plot_df_2$V4 <- ifelse(chr_plot_df_2$V3.x+150000<=chr_plot_df_2$V3.y,chr_plot_df_2$V3.x+150000,chr_plot_df_2$V3.y)
+chr_plot_df_2$V4 <- ifelse(chr_plot_df_2$V3.x+500000<=chr_plot_df_2$V3.y,chr_plot_df_2$V3.x+500000,chr_plot_df_2$V3.y)
 
 
 #--------------
@@ -104,13 +104,13 @@ chr_plot_df_2$V4 <- ifelse(chr_plot_df_2$V3.x+150000<=chr_plot_df_2$V3.y,chr_plo
 chrom_bg+
   geom_segment(data=chr_plot_df_2[which(chr_plot_df_2$group=="gain"),],
                aes(x=V1,xend=V1,y=V2.x,yend=V4,color="gain"),
-                      size = 3,position=position_nudge(x = 0.25))+
+                      size = 4,position=position_nudge(x = 0.25))+
   geom_segment(data=chr_plot_df_2[which(chr_plot_df_2$group=="both"),],
                aes(x=V1,xend=V1,y=V2.x,yend=V4,color="both"),
-               size = 3,position=position_nudge(x = 0))+
+               size = 4,position=position_nudge(x = 0))+
   geom_segment(data=chr_plot_df_2[which(chr_plot_df_2$group=="loss"),],
                aes(x=V1,xend=V1,y=V2.x,yend=V4,color="loss"),
-               size = 3,position=position_nudge(x = -0.25))+
+               size = 4,position=position_nudge(x = -0.25))+
   scale_color_manual(values = c("gain"="#762a83",
                                 "both"="#1b7837",
                                 "loss"="#e08214"))+
@@ -118,23 +118,23 @@ chrom_bg+
 
 #--------------
 # V1
-
-chrom_bg+
-  geom_segment(data=chr_plot_df_2,aes(x=V1,xend=V1,y=V2.x,yend=V4,color=group),
-               size = 4)+
-  scale_color_manual(values = c(gain="#762a83",
-                                both="#1b7837",
-                                loss="#e08214"))+
-  coord_flip()
-
-
-# dark bg
-chrom_bg_2+geom_segment(data=chr_plot_df_2,aes(x=V1,xend=V1,y=V2.x,yend=V4,color=group),
-                      size = 4)+
-  scale_color_manual(values = c(gain="#762a83",
-                                both="#4d9221",
-                                loss="#e08214"))+
-  coord_flip()
+#
+# chrom_bg+
+#   geom_segment(data=chr_plot_df_2,aes(x=V1,xend=V1,y=V2.x,yend=V4,color=group),
+#                size = 4)+
+#   scale_color_manual(values = c(gain="#762a83",
+#                                 both="#1b7837",
+#                                 loss="#e08214"))+
+#   coord_flip()
+#
+#
+# # dark bg
+# chrom_bg_2+geom_segment(data=chr_plot_df_2,aes(x=V1,xend=V1,y=V2.x,yend=V4,color=group),
+#                       size = 4)+
+#   scale_color_manual(values = c(gain="#762a83",
+#                                 both="#4d9221",
+#                                 loss="#e08214"))+
+#   coord_flip()
 
 
 
